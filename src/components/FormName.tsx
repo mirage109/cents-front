0468be3e5aed16dc22a38ebe39903218c9e4fc8d
Anastasiia Zibla <zibla.asia@gmail.com>
2024-04-10 13:18:02 +0200

    feat: formName receive the name and pass it forward

--- src/components/FormName.tsx
+++ src/components/FormName.tsx
@@ -1,11 +1,37 @@
-const FormName = () => {
-  return (
-    <div className="flex flex-col">
-      <label>Hi! Please enter your name!</label>
-      <input type="text" placeholder="Your name" />
-      <button>Next Step</button>
-    </div>
+import React, { useState } from "react";
 
+interface FormNameProps {
+  onSubmit: (name: string) => void;
+}
+
+const FormName: React.FC<FormNameProps> = ({ onSubmit }) => {
+  const [name, setName] = useState("");
+
+  const handleSubmit:React.MouseEventHandler<HTMLButtonElement> 
+   = (event) => {
+    event.preventDefault();
+    onSubmit(name);
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <form >
+      <div className="flex flex-col">
+        <label>Hi! Please enter your name!</label>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={name}
+          onChange={handleNameChange}
+        />
+        <button type="button" onClick={handleSubmit}>
+          Next Step
+        </button>
+      </div>
+    </form>
   );
 };
 
--- src/components/FormNumbers.tsx
+++ src/components/FormNumbers.tsx
@@ -1,7 +1,10 @@
-const FormNumbers = () => {
+interface FormNumbersProps {
+  name: string;
+}
+const FormNumbers:React.FC<FormNumbersProps> = ({name}) => {
   return (
     <div className="flex flex-col">
-      <label>What are your favourite numbers?</label>
+      <label>{name} What are your favourite numbers?</label>
       <input type="text" placeholder="Your first number " />
       <input type="text" placeholder="Your second number" />
       <button>Next Step</button>
