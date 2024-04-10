58dc0c3097c1f7150589d30a7d393b127a049e26
Anastasiia Zibla <zibla.asia@gmail.com>
2024-04-10 14:28:15 +0200

    feat: numbers are correcly summed

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,16 +6,48 @@ import { useState } from "react";
 const Form = () => {
   const [step, setStep] = useState(1);
   const [name, setName] = useState("");
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [sum, setSum] = useState(0);
+  const [showResult, setShowResult] = useState(false);
 
-    const handleFormSubmit = (name: string) => {
-        setName(name);
-        setStep(step + 1);
-    };
+
+  const handleFormSubmit = (name: string) => {
+    setName(name);
+    setStep(step + 1);
+  };
+
+  const handleNumbersSubmit = async () => {
+    const response = await fetch("http://localhost:3300/calculate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        num1: parseInt(number1),
+        num2: parseInt(number2),
+      }),
+    });
+    const data = await response.json();
+    setSum(data.result);
+    setShowResult(true);
+    setStep(step + 1)
+  };
+
   return (
     <div className="max-w-[40%] ">
-        {step === 1 && <FormName onSubmit={handleFormSubmit} />}
-        {step === 2 && <FormNumbers name={name} />}
-        {step === 3 && <FormResult />}
+      {step === 1 && <FormName onSubmit={handleFormSubmit} />}
+      {step === 2 && (
+        <FormNumbers
+          name={name}
+          onSubmit={() => handleNumbersSubmit()}
+          onNumber1Change={(value) => setNumber1(value)}
+          onNumber2Change={(value) => setNumber2(value)}
+        />
+      )}
+       {showResult && (
+        <FormResult name={name} number1={number1} number2={number2} sum={sum} />
+        )}
     </div>
   );
 };
--- src/components/FormNumbers.tsx
+++ src/components/FormNumbers.tsx
@@ -1,13 +1,37 @@
+import React, { useState } from "react";
+
 interface FormNumbersProps {
   name: string;
+  onSubmit: () => void;
+  onNumber1Change: (value: string) => void;
+  onNumber2Change: (value: string) => void;
 }
-const FormNumbers:React.FC<FormNumbersProps> = ({name}) => {
+const FormNumbers:React.FC<FormNumbersProps> = ({name, onSubmit, onNumber1Change, onNumber2Change}) => {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
   return (
     <div className="flex flex-col">
-      <label>{name} What are your favourite numbers?</label>
-      <input type="text" placeholder="Your first number " />
-      <input type="text" placeholder="Your second number" />
-      <button>Next Step</button>
+      <h3>{name}, please enter two numbers</h3>
+      <p>I will add them and add a third random number to the sum.</p>
+    <input
+        type="number"
+        placeholder="Your first number"
+        value={number1}
+        onChange={(e) => {
+          setNumber1(e.target.value);
+          onNumber1Change(e.target.value);
+        }}
+      />
+      <input
+        type="number"
+        placeholder="Your second number"
+        value={number2}
+        onChange={(e) => {
+          setNumber2(e.target.value);
+          onNumber2Change(e.target.value);
+        }}
+      />
+      <button onClick={onSubmit}>Next Step</button>
     </div>
   );
 };
