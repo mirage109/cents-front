d0e01c1237508f2ee165af94ade1e24457dbb832
Anastasiia Zibla <zibla.asia@gmail.com>
2024-04-11 16:03:43 +0200

    feat: additional logic and restart

--- src/components/FormResult.tsx
+++ src/components/FormResult.tsx
@@ -3,6 +3,7 @@ interface FormResultProps {
   number1: string;
   number2: string;
   sum: number;
+  onClick: () => void;
 }
 
 const FormResult: React.FC<FormResultProps> = ({
@@ -10,16 +11,28 @@ const FormResult: React.FC<FormResultProps> = ({
   number1,
   number2,
   sum,
+  onClick: handleRevial,
 }) => {
   return (
     <div className="p-10 font-bebas text-xl flex flex-col justify-center items-center gap-4 text-primary">
-      <h3><span className="text-secondary">{name}</span>, your numbers were:</h3>
-      <p  className="font-bold text-3xl text-secondary"
-      >
-        {number1} <span className="text-xl text-primary font-normal"> and</span> {number2}
+      <h3>
+        <span className="text-secondary">{name}</span>, your numbers were:
+      </h3>
+      <p className="font-bold text-3xl text-secondary">
+        {number1} <span className="text-xl text-primary font-normal"> and</span>{" "}
+        {number2}
       </p>
-      <p>And the sum is: <span className="font-bold text-3xl text-secondary">{sum}</span></p>
+      <p>
+        And the sum is:{" "}
+        <span className="font-bold text-3xl text-secondary">{sum}</span>
+      </p>
       <p>So, What was the third number? </p>
+      <button
+        onClick={handleRevial}
+        className="border-2 border-secondary rounded-md p-2 bg-secondary text-white w-2/3 hover:bg-white hover:text-secondary hover:border-secondary transition duration-500 ease-in-out"
+      >
+        Revial
+      </button>
     </div>
   );
 };
